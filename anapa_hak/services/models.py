01f0eca20e6059921fdfb00ea7527582d90349e0
nocticron <nocticron@yandex.ru>
2022-05-22 01:20:51 +0300

    patches the models: char fields

--- anapa_hak/services/models.py
+++ anapa_hak/services/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.core.validators import (MaxValueValidator, MinValueValidator)
 
-MENU_TYPES = ['Breakfast', 'Dinner', 'Supper']
+MENU_TYPES = [('Bre', 'Завтрак'), ('Din', 'Обед'), ('Sup', 'Ужин')]
 
-FOODS = ['Food1', 'Food2', 'Food3']
+FOODS = [('Food1', 'Супчик'), ('Food2', 'Булка'), ('Food3', 'Фрукты')]
 
 
 class Menu(models.Model):
     date = models.DateTimeField('Время и дата')
-    menu_type = models.CharField(MENU_TYPES)
-    dishes = models.CharField(FOODS)
+    menu_type = models.CharField('Вид меню', choices=MENU_TYPES, max_length=3)
+    dishes = models.CharField('Блюда',choices=FOODS, max_length=32)
     # dishes = models.ManyToManyField(Food, related_name='Menus')
 
 class Activity(models.Model):
-    name = models.CharField('Название')
+    name = models.CharField('Название', max_length=128)
     responsible = models.ForeignKey('Supervisor', on_delete=models.CASCADE, related_name='activities')
     date_start = models.DateTimeField('Начало')
     date_end = models.DateTimeField('Начало')
